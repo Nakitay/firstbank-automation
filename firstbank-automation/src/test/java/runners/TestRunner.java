f32c93773da8b438dd17047a4ee76666246aec45
Nargiza Umali <Nakitay84@gmail.com>
2026-03-13 14:45:03 -0600

    Add GitHub CI pipeline changed

--- firstbank-automation/src/test/java/runners/TestRunner.java
+++ firstbank-automation/src/test/java/runners/TestRunner.java
@@ -4,11 +4,12 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "classpath:features",
+        features = "src/test/resources/features",
         glue = "stepDefinitions",
         plugin = {
                 "pretty",
-                "html:target/cucumber-report.html"
+                "html:target/cucumber-report.html" ,
+                        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
         },
         monochrome = true
 )
--- firstbank-automation/src/test/java/ui/pages/LoginPage.java
+++ firstbank-automation/src/test/java/ui/pages/LoginPage.java
@@ -1,18 +1,32 @@
 package ui.pages;
 
+import ui.pages.BasePage;
+import utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 public class LoginPage extends BasePage {
 
-        @FindBy(id="username")
-        public WebElement username;
+    @FindBy(id="username")
+    public WebElement username;
 
-        @FindBy(id="password")
-        public WebElement password;
+    @FindBy(id="password")
+    public WebElement password;
 
-        @FindBy(id="login")
-        public WebElement loginButton;
+    @FindBy(id="login")
+    public WebElement loginButton;
 
+    public void login(String user, String pass){
+
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+
+        wait.until(ExpectedConditions.visibilityOf(username));
+
+        username.sendKeys(user);
+        password.sendKeys(pass);
+        loginButton.click();
     }
-
+}
